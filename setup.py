60769d15317eacc3132a9c4b14c469486b824af7
Fernando Macedo <fgmacedo@gmail.com>
2013-04-22 14:53:16 -0300

    preparing for pypi

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@
 
 from setuptools import setup, find_packages
 
+with open('README.rst') as f:
+    long_description = f.read()
+
 setup(
     name='NoseGrowlNotify',
     author='Fernando Macedo',
     author_email='fgmacedo@gmail.com',
     description='nose plugin for Growl notifications',
+    long_description=long_description,
+    licence='Creative Commons Attribution license (CC BY)',
     install_requires=['nose>=0.10', 'gntp'],
     url="http://github.org/fgmacedo/nose-growl-notify",
     version='0.1',
@@ -18,6 +23,6 @@ setup(
     },
     entry_points={
         'nose.plugins.0.10': [
-            'nosegrowl = nosegrowl:NoseGrowlNotify']
+            'nosegrowlnotify = nosegrowlnotify:NoseGrowlNotify']
     }
 )
